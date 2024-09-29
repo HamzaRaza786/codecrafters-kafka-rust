3d9771779abd888d0f081838a5765115fdc3599a
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-09-30 01:45:25 +0500

    nv3: send correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{prelude::*, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    str::from_utf8,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,16 +12,21 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     //
-    // let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+                handle_client(_stream);
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
+fn handle_client(mut stream: TcpStream) {
+    let response: i64 = 2_i64.pow(32) + 7_i64;
+    stream.write_all(&response.to_be_bytes()).unwrap();
 }
