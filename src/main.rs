2b94acf4a8045516db16e8411b35923bf8d66239
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-10-05 02:47:17 +0500

    Parse Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ use std::{
     io::{prelude::*, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
     str::from_utf8,
+    string,
 };
+
+struct Headers {
+    request_api_key: i16,
+    request_api_version: i16,
+    correlation_id: i32,
+    client_id: Option<String>,
+    tagged_fields: Option<String>,
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -27,6 +36,10 @@ fn main() {
     }
 }
 fn handle_client(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let mut lines = buf_reader.lines();
+    let request_line = lines.next().unwrap().unwrap();
+    print!("{:?}", request_line);
     let response: i64 = 2_i64.pow(32) + 7_i64;
     stream.write_all(&response.to_be_bytes()).unwrap();
 }
