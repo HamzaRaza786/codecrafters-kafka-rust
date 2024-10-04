8b6901858f818da7207d79821beb5092c06e4d49
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-10-05 03:44:06 +0500

    My own

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![allow(unused_imports)]
+use bytes::{Buf, BufMut};
 use std::{
-    fs,
-    io::{prelude::*, BufReader, Read, Write},
+    any, fs,
+    io::{prelude::*, BufReader, Cursor, Read, Seek, Write},
     net::{TcpListener, TcpStream},
     str::from_utf8,
     string,
 };
-
 struct Headers {
     request_api_key: i16,
     request_api_version: i16,
@@ -36,10 +36,19 @@ fn main() {
     }
 }
 fn handle_client(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let mut lines = buf_reader.lines();
-    let request_line = lines.next().unwrap().unwrap();
-    print!("{:?}", request_line);
-    let response: i64 = 2_i64.pow(32) + 7_i64;
-    stream.write_all(&response.to_be_bytes()).unwrap();
+    // let mut buf_reader = BufReader::new(&mut stream);
+
+    let mut len = [0; 4];
+    stream.read_exact(&mut len).unwrap();
+    let len = i32::from_be_bytes(len) as usize;
+    let mut request = vec![0; len];
+    stream.read_exact(&mut request).unwrap();
+    let mut request = request.as_slice();
+    let _request_api_key = request.get_i16();
+    let _request_api_version = request.get_i16();
+    let correlation_id = request.get_i32();
+    let mut response = Vec::with_capacity(8);
+    response.put_i32(0);
+    response.put_i32(correlation_id);
+    stream.write_all(&response).unwrap();
 }
