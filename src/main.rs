00e0b78f870192ad2608aef53787e73ded4478bf
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-10-08 03:16:18 +0500

    Fixed Implementation for Handle Api version

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,17 @@ struct Headers {
     client_id: Option<String>,
     tagged_fields: Option<String>,
 }
+struct Body {
+    correlation_id: i32,
+    error_code: i16,
+    num_api_keys_records: i16,
+    api_key: i16,
+    min_version: i16,
+    max_version: i16,
+    tag_buffer: i8,
+    throttle_time_ms: i32,
+    tag_buffer_length: i8,
+}
 
 fn message_length(mut stream: &TcpStream) -> usize {
     let mut buffer = [0; 4];
@@ -43,6 +54,26 @@ fn request_headers(mut stream: &TcpStream) -> Headers {
         tagged_fields: None,
     }
 }
+fn construct_response(request_headers: Headers) -> Body {
+    let error_code;
+
+    if !(0..5).contains(&request_headers.request_api_version) {
+        error_code = ErrorCodes::UnsupportedVersion as i16;
+    } else {
+        error_code = 0;
+    }
+    return Body {
+        correlation_id: request_headers.correlation_id,
+        error_code,
+        num_api_keys_records: 2,
+        api_key: request_headers.request_api_key,
+        min_version: 0,
+        max_version: 4,
+        tag_buffer: 0,
+        throttle_time_ms: 420,
+        tag_buffer_length: 0,
+    };
+}
 
 fn handle_client(mut stream: TcpStream) {
     let request_headers = request_headers(&stream);
@@ -53,11 +84,15 @@ fn handle_client(mut stream: TcpStream) {
     } else {
         response_body.put_i16(0);
     }
-    response_body.put_i32(1);
-    response_body.put_i32(18);
-    response_body.put_i32(4);
+    response_body.put_i8(2); // num api key records + 1
+    response_body.put_i16(18); // api key
+    response_body.put_i16(0); // min version
+    response_body.put_i16(4); // max version
+    response_body.put_i8(0); // TAG_BUFFER
+    response_body.put_i32(420); // throttle time ms
+    response_body.put_i8(0); // TAG_BUFFER length
     let response_message_length = response_body.len() as i32;
-    let mut response = Vec::with_capacity(4 + response_body.len());
+    let mut response = Vec::with_capacity(RESPONSE_LENGTH + response_body.len());
     response.put_i32(response_message_length);
     response.extend(response_body);
     stream.write_all(&response).unwrap();
